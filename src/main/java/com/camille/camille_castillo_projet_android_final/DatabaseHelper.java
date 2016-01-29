26ca08de0fffa750d4499045729e69feb91996da
Camille Castillo <camillecastillo15@gmail.com>
2016-01-30 00:24:11 +0100

    database fragment fonctionne

--- src/main/java/com/camille/camille_castillo_projet_android_final/DatabaseHelper.java
+++ src/main/java/com/camille/camille_castillo_projet_android_final/DatabaseHelper.java
@@ -9,9 +9,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by Camille on 06/01/2016.
  */
+
+//DatabaseHelper est une classe qui va nous aider à exécuter des opérations sur notre Base de Données : créer, mettre à jour, voir, supprimmer des données
+//Elle doit hériter de la classe SQLiteOpenHelper qui permet de gérer SQLite avec Androïd
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    //Pour créer une Base de Données avec Androïd, nous avons besoin tout d'abord d'un nom pour la BDD
+    //Ce nom sera un attribut de notre classe, tout comme le nom de la table et des colonnes
     public static final String DATABASE_NAME = "student.db";
+    //Ensuite un nom de table
     public static final String TABLE_NAME = "student_table";
+    //Et un nom pour chaque colonne
+    //ID représente la clé primaire de la table
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
     public static final String COL_3 = "SURNAME";
@@ -20,60 +30,106 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(Context contex){
 
         //La Base de Données est crée quand le constructeur est appelé
+        //La super classe a besoin du context
+        // du nom de la BDD,
+        // du comportement de base du cursorFactory soit celui de base car null,
+        // et de la version de la BDD ici, donc 1
         super(contex, DATABASE_NAME, null, 1);
-        //SQLiteDatabase db = this.getWritableDatabase();
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        //execSQL indique une requête que la BDD va exécuter dès qu'elle est crée
+        //Ici une table portant le nom de notre va être crée,
+        //Un + qui est un opérateur de concaténation marque la différence entre TABLE_NAME qui est une variable Java, et le reste, de type String
+        // prenant comme colonne ID de type entier, qui s'autoincrémente et est clé primaire,
+        //Name de type text,
+        //Surname de type text,
+        //Et marks de type entier
         db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS INTEGER) ");
 
     }
 
     @Override
+    //Méthode permettant de passer à la version supérieure d'une BDD
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        //La table se détruit si elle existe puis est remplacé
         db.execSQL("DROP TABLE IF EXISTS"+TABLE_NAME);
+        //La méthode onCreate avec la nouvelle BDD comme argument est appelée et exécutée
         onCreate(db);
 
     }
 
-    public boolean inserData(String name, String surname, String marks){
+    //Méthode pour insérer des données dans la BDD
+    //Elle prend en argument toutes les colonnes sauf ID qui va être crée automatiquement
+    public boolean insertData(String name, String surname, String marks){
 
+        //Une instance de SQLiteDatabase est crée, nommée db
+        //Cette BDD pourra être modifiée (writable)
         SQLiteDatabase db = this.getWritableDatabase();
 
+        //Une instance de classe ContentValeus est crée
+        //Elle perment de ranger des données à l'intérieur
+        //Comme des strings, des variables...
         ContentValues contentValues = new ContentValues();
 
+        //Ici on range donc dans contentvalues chaque donnée de colonne,
+        // avec le nom de la colonne
         contentValues.put(COL_2, name);
         contentValues.put(COL_3, surname);
         contentValues.put(COL_4, marks);
 
+        //insert prend 3 paramètres,
+        // le nom de table,
+        //En deuxième un paramètre qui doit être égal à null,
+        // Sauf si on désire passer une valeur nulle à une colonne,
+        //Car SQLite ne permet pas d'insérer que des colonnes vides
+        // les données à insérer avec les nom des colonnes et leurs valeurs
+        //La méthode est sauvegardée dans une long variable
+        //Car elle retourne -1 si erreur
+        //0 sinon
         long result = db.insert(TABLE_NAME, null, contentValues);
 
+        //La méhtode retournera false si elle n'a pas marché
         if(result == -1) return false;
+        //Sinon true
         else return true;
 
     }
 
     public Cursor getAllData(){
 
+        //Une instance de SQLiteDatabase est crée, nommée db
+        //Cette BDD pourra être modifiée (writable)
         SQLiteDatabase db = this.getWritableDatabase();
 
+        //rawQuery permet de créer une requête pour notre base
+        //Cursor représente une collections de données relatives à une BDD (les lignes)
+        //Ici toutes les lignes de la table sont retournée (par exemple 1 Camille Castillo 80)
         Cursor res = db.rawQuery("select * from "+TABLE_NAME, null);
 
-        //Retour de la fonction (cursor)
+        //La fonction attend un objet de type Cursor en retour
+        //Il est ici retourné
         return res;
 
     }
 
     public boolean updateData(String id, String name, String surname, String marks) {
 
+        //Une instance de SQLiteDatabase est crée, nommée db
+        //Cette BDD pourra être modifiée (writable)
         SQLiteDatabase db = this.getWritableDatabase();
 
+        //Une instance de classe ContentValeus est crée
+        //Elle perment de ranger des données à l'intérieur
+        //Comme des strings, des variables...
         ContentValues contentValues = new ContentValues();
 
+        //Ici on range donc dans contentvalues chaque donnée de colonne,
+        // avec le nom de la colonne
         contentValues.put(COL_1, id);
         contentValues.put(COL_2, name);
         contentValues.put(COL_3, surname);
@@ -81,17 +137,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         //4 arguments pour updater une table de la BDD
         //nom de la table, la colonne à modifier et son contenu, la condition
+        //"ID == ?" indique que la colonne doit se modifier suivant
         db.update(TABLE_NAME, contentValues, "ID == ?", new String[] {id});
 
+        //La fonction doit retourner un booléen (ici true)
         return true;
 
     }
 
     public Integer deleteData(String id){
 
+        //Une instance de SQLiteDatabase est crée, nommée db
+        //Cette BDD pourra être modifiée (writable)
         SQLiteDatabase db = this.getWritableDatabase();
 
         //La fonction delete prend 3 arguments
+        //Elle supprimme les valeurs
         //Le nom de la table, le nom du début des colonnes à supprimer (ici depuis l'id)
         return db.delete(TABLE_NAME, "ID == ?", new String[] {id});
 
--- src/main/java/com/camille/camille_castillo_projet_android_final/SQLiteFragment.java
+++ src/main/java/com/camille/camille_castillo_projet_android_final/SQLiteFragment.java
@@ -17,15 +17,18 @@ import android.widget.Toast;
  */
 public class SQLiteFragment extends Fragment {
 
+    //Un instance de la classe DatabaseHelper est crée, nommée myDb
     DatabaseHelper myDb;
+    //Les editText du layout sont placés dans des instances d'EditText
     EditText editName, editSurname, editMarks, editTextId;
+    //Pareil pour les boutons du fragment
     Button btnAddData;
     Button btnviewAll;
     Button btnUpdate;
     Button btnDelete;
 
     public SQLiteFragment() {
-        // Required empty public constructor
+        // Un constructeur vide est requis
     }
 
 
@@ -33,23 +36,28 @@ public class SQLiteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View _vue =  inflater.inflate(R.layout.fragment_sqlite, container, false);
-        _vue.clearFocus();
 
+        //Ici elle est crée, elle a besoin du contexte comme argument,
+        // on lui renvoit celui de l'activité
+        //Pour vérifier si la BDD a bien été crée avec les bonnes tables / colonnes
+        //Il est possible de se rendre dans l'Androïd Device Monitor quand l'émulateur et l'application tournent
+        //Sous le nom de notre application, on peut trouver les éléments crées, on retrouve alors notre BDD
+        //Il est même possible de l'exporter et de la gérer avec un gestionnaire de BDD de type SQLite
         myDb = new DatabaseHelper(this.getActivity());
 
+        //Les instances d'editTexts sont reliés aux boutons du layout
         editName = (EditText)_vue.findViewById(R.id.editText_name);
-        editName.clearFocus();
         editSurname = (EditText)_vue.findViewById(R.id.editText_surname);
-        editSurname.clearFocus();
         editMarks = (EditText)_vue.findViewById(R.id.editText_marks);
-        editMarks.clearFocus();
         editTextId = (EditText)_vue.findViewById(R.id.editText_id);
 
+        //Pareil pour les boutons
         btnAddData = (Button)_vue.findViewById(R.id.button_add);
         btnviewAll = (Button)_vue.findViewById(R.id.button_viewall);
         btnUpdate = (Button)_vue.findViewById(R.id.button_update);
         btnDelete = (Button)_vue.findViewById(R.id.button_delete);
 
+        //Les méthodes d'ajouts, de vu, de mis à jour ou encore de suppresion sont exécutés au lancement du fragment
         AddData();
         viewAll();
         UpdateData();
@@ -66,9 +74,15 @@ public class SQLiteFragment extends Fragment {
                     @Override
                     public void onClick(View v) {
 
+                        //La fonction deleteData de DatabaseHelper est appelée,
+                        //Les lignes à identifier seront identifiés par leur ID
+                        //Le résultat est enregistré dans un entier nommé "deletedRows"
                         Integer deletedRows = myDb.deleteData(editTextId.getText().toString());
 
+                        //La fonction retournera 0 si cela fonctionne
+                        //Ce qui créera un Toast avec un message positif
                         if(deletedRows > 0) Toast.makeText(getActivity(), "Les données ont été supprimées", Toast.LENGTH_LONG).show();
+                            //Sinon le message sera différent
                         else Toast.makeText(getActivity(), "Les données n'ont pas été supprimées", Toast.LENGTH_LONG).show();
 
                     }
@@ -86,10 +100,14 @@ public class SQLiteFragment extends Fragment {
                     @Override
                     public void onClick(View v) {
 
+                        //La fonction updateData est appelée sur notre BDD
+                        //Les contenus des editText du layout sont pris en arguments
+                        //Ils ont besoin d'être convertis en String pour correspondre aux types d'arguments de la fonctions
                         boolean isUpdated = myDb.updateData(editTextId.getText().toString(),
                                                             editName.getText().toString(),
                                                             editSurname.getText().toString(),
                                                             editMarks.getText().toString());
+
 
                         if(isUpdated) Toast.makeText(getActivity(), "Les données sont mises à jour", Toast.LENGTH_LONG).show();
                         else Toast.makeText(getActivity(), "Les données ne sont pas mises à jour", Toast.LENGTH_LONG).show();
@@ -103,12 +121,23 @@ public class SQLiteFragment extends Fragment {
 
     public void AddData(){
 
+        //Un écouteur de clic est placé sur le bouton d'ajout de données
         btnAddData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               boolean isInserted =  myDb.inserData(editName.getText().toString(), editSurname.getText().toString(), editMarks.getText().toString());
 
+               //Un booléen est crée suivant l'insertion des données
+               // qui sont égales aux textes des instances d'editTexts (converties en String)
+               boolean isInserted =  myDb.insertData(editName.getText().toString(), editSurname.getText().toString(), editMarks.getText().toString());
+
+                //Si le booléen est égale à true
+                //Un toast est affiché qui indiques que les données ont bien été insérées
+                //Un toast prend en paramètre,
+                // un context, une string,
+                // un type de taille
+                //Enfin la méthode show() est appelée sur le Toast pour l'afficher
                 if(isInserted) Toast.makeText(getActivity(), "Les données sont insérées", Toast.LENGTH_LONG).show();
+                //Sinon un autre toast renvoit le contraire
                 else Toast.makeText(getActivity(), "Les données ne sont pas insérées", Toast.LENGTH_LONG).show();
 
             }
@@ -118,6 +147,7 @@ public class SQLiteFragment extends Fragment {
 
     public void viewAll(){
 
+        //Un écouteur de clic est posé sur le btnviewALl
         btnviewAll.setOnClickListener(
 
                 new View.OnClickListener(){
@@ -125,27 +155,44 @@ public class SQLiteFragment extends Fragment {
                     @Override
                     public void onClick(View v){
 
+                        //La fonction de DatabaseHelper est appelée
+                        //Elle permet de ranger toutes les lignes de notre database dans un cursor
                        Cursor res = myDb.getAllData();
+                        //Si la taille du cursor est égale à 0
+                        //Cela signifie que l'opération n'a pas marché
                         if(res.getCount() == 0){
 
-                            //afficher les message
+                            //Cela va créer un messaage d'erreur
+                            //Renvoyé par la fonction showMessage
                             showMessage("Error", "Nothing found");
+                            //La fonction s'arrête
                             return;
 
                         }
 
+                        //S'il y a un résultat
+                        //On crée une instance de StringBuffer nommée buffer
+                        //StringBuffer est une string mise en tampon
+                        //Cela veut dire qu'elle va être détruite plus tard
                         StringBuffer buffer = new StringBuffer();
 
+                        //La fonction moveToNext permet de déplacer le cursor au résultat suivant
                         while(res.moveToNext()){
+
+                            //La string buffer va ajouter progressivement les résultats de la requête
+                            //Avec le nom de la bonne colonne au début
+                            //+ \n qui permet un retour à la ligne,
 
                             buffer.append("Id : " + res.getString(0) + "\n");
                             buffer.append("Name : "+ res.getString(1)+ "\n");
                             buffer.append("Surname : "+ res.getString(2)+ "\n");
+
+                            //Ou quand il est double, un saut de ligne
                             buffer.append("Marks : "+ res.getString(3)+ "\n\n");
 
                         }
 
-                        //Show all datas
+                        //La fonction showMessage est appelée
                         showMessage("Data", buffer.toString());
 
                     }
@@ -159,11 +206,19 @@ public class SQLiteFragment extends Fragment {
 
     public void showMessage(String title, String Message){
 
+        //Une alerte dialogue est une alerte qui apparaît sur l'écran
+        //Son thème est celui par défaut, défini par Builder
         AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
+        //Elle peut disparaître si setCancelable est à ture (si on clique en dehors de l'alerte)
         builder.setCancelable(true);
+        //Son titre peut être celui d'une variable
         builder.setTitle(title);
+        //Son message aussi
+        //Ici ce sera les données de note BDD
+        //Car ces infos sont passées en paramètres quand showMessage est appelé dans la fonction viewAll()
         builder.setMessage(Message);
 
+        //Le builder va être affiché
         builder.show();
 
     }
